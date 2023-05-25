7103aa2dbbe85819a01755416433ef5c5ed4b9df
alaaelqady <alaaelqady@gmail.com>
2023-05-26 02:03:54 +0300

    ezik

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,4 +1,5 @@
-import 'package:Despoky/shared/header.dart';
+import 'package:Despoky/shared/appbar.dart';
+import 'package:appbar_animated/appbar_animated.dart';
 import 'package:flutter/material.dart';
 import 'package:Despoky/utilities/routes.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -58,129 +59,136 @@ class _ProfileScreenState extends State<ProfileScreen>
 
   @override
   Widget build(BuildContext context) {
-    return Sizer(
-      builder: (context, orientation, deviceType) {
-        return Scaffold(
-          backgroundColor: Color(0xFF171725),
-          body: SingleChildScrollView(
-            child: Column(
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(left: 7.w, right: 7.w),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: <Widget>[
-                      SizedBox(height: 2.h),
-                      Header(),
-                      ScaleTransition(
-                        scale: _animation,
-                        child: CircleAvatar(
-                          backgroundColor: Colors.white,
-                          radius: 50.sp,
-                          child: Icon(Icons.person,
-                              size: 50.sp, color: Color(0xFF171725)),
-                        ),
-                      ),
-                      SizedBox(height: 5.h),
-                      TyperAnimatedTextKit(
-                        text: ['Hello, $_userName!'],
-                        textStyle: GoogleFonts.tenorSans(
-                          textStyle: TextStyle(
-                              fontSize: 20.sp,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white),
-                        ),
-                        speed: Duration(milliseconds: 200),
-                        textAlign: TextAlign.center,
-                      ),
-                      SizedBox(height: 8.h),
-                      Text(
-                        _userEmail,
-                        style: GoogleFonts.lato(
-                          textStyle:
-                          TextStyle(fontSize: 14.sp, color: Colors.white),
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                      SizedBox(height: 3.h),
-                      Text(
-                        _userPhoneNumber,
-                        style: GoogleFonts.lato(
-                          textStyle:
-                          TextStyle(fontSize: 14.sp, color: Colors.white),
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                      SizedBox(height: 8.h),
-                      Container(
-                        height: 8.h,
-                        child: TextButton(
-                          style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.all<Color>(
-                              Colors.white,
-                            ),
-                            shape: MaterialStateProperty.all<
-                                RoundedRectangleBorder>(
-                              RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10.sp),
-                              ),
-                            ),
+    return ScaffoldLayoutBuilder(
+      backgroundColorAppBar:
+      const ColorBuilder(Colors.transparent, Color(0xFF171725)),
+      textColorAppBar: const ColorBuilder(Colors.white),
+      appBarBuilder: (context, colorAnimated) => CustomAppBar(
+        colorAnimated: colorAnimated,
+      ),
+      child: Sizer(
+        builder: (context, orientation, deviceType) {
+          return Scaffold(
+            backgroundColor: Color(0xFF171725),
+            body: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(left: 7.w, right: 7.w),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        SizedBox(height: 15.h),
+                        ScaleTransition(
+                          scale: _animation,
+                          child: CircleAvatar(
+                            backgroundColor: Colors.white,
+                            radius: 50.sp,
+                            child: Icon(Icons.person,
+                                size: 50.sp, color: Color(0xFF171725)),
                           ),
-                          onPressed: () {
-                            Navigator.of(context, rootNavigator: true)
-                                .pushNamed(AppRoutes.editProfilePageRoute);
-                          },
-                          child: Text(
-                            'Edit Profile',
-                            style: GoogleFonts.lato(
-                              textStyle: TextStyle(
-                                fontSize: 14.sp,
+                        ),
+                        SizedBox(height: 5.h),
+                        TyperAnimatedTextKit(
+                          text: ['Hello, $_userName!'],
+                          textStyle: GoogleFonts.tenorSans(
+                            textStyle: TextStyle(
+                                fontSize: 20.sp,
                                 fontWeight: FontWeight.bold,
-                                color: Color(0xFF171725),
+                                color: Colors.white),
+                          ),
+                          speed: Duration(milliseconds: 200),
+                          textAlign: TextAlign.center,
+                        ),
+                        SizedBox(height: 8.h),
+                        Text(
+                          _userEmail,
+                          style: GoogleFonts.lato(
+                            textStyle:
+                            TextStyle(fontSize: 14.sp, color: Colors.white),
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        SizedBox(height: 3.h),
+                        Text(
+                          _userPhoneNumber,
+                          style: GoogleFonts.lato(
+                            textStyle:
+                            TextStyle(fontSize: 14.sp, color: Colors.white),
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        SizedBox(height: 8.h),
+                        Container(
+                          height: 8.h,
+                          child: TextButton(
+                            style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all<Color>(
+                                Colors.white,
+                              ),
+                              shape: MaterialStateProperty.all<
+                                  RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10.sp),
+                                ),
+                              ),
+                            ),
+                            onPressed: () {
+                              Navigator.of(context, rootNavigator: true)
+                                  .pushNamed(AppRoutes.editProfilePageRoute);
+                            },
+                            child: Text(
+                              'Edit Profile',
+                              style: GoogleFonts.lato(
+                                textStyle: TextStyle(
+                                  fontSize: 14.sp,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xFF171725),
+                                ),
                               ),
                             ),
                           ),
                         ),
-                      ),
-                      SizedBox(height: 3.h),
-                      Container(
-                        height: 8.h,
-                        child: TextButton(
-                          style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.all<Color>(
-                              Colors.redAccent,
-                            ),
-                            shape: MaterialStateProperty.all<
-                                RoundedRectangleBorder>(
-                              RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10.sp),
+                        SizedBox(height: 3.h),
+                        Container(
+                          height: 8.h,
+                          child: TextButton(
+                            style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all<Color>(
+                                Colors.redAccent,
+                              ),
+                              shape: MaterialStateProperty.all<
+                                  RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10.sp),
+                                ),
                               ),
                             ),
-                          ),
-                          onPressed: () {
-                            Navigator.of(context, rootNavigator: true)
-                                .pushNamed(AppRoutes.deleteProfilePageRoute);
-                          },
-                          child: Text(
-                            'Delete Account',
-                            style: GoogleFonts.lato(
-                              textStyle: TextStyle(
-                                fontSize: 14.sp,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white,
+                            onPressed: () {
+                              Navigator.of(context, rootNavigator: true)
+                                  .pushNamed(AppRoutes.deleteProfilePageRoute);
+                            },
+                            child: Text(
+                              'Delete Account',
+                              style: GoogleFonts.lato(
+                                textStyle: TextStyle(
+                                  fontSize: 14.sp,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
                               ),
                             ),
                           ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        );
-      },
+          );
+        },
+      ),
     );
   }
 }
